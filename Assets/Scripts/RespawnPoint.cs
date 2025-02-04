1c9cdd646363a113ddd12fa0680df228bb9c37fc
GameerGuy <danieloseinimo@gmail.com>
2025-02-04 17:32:24 +0000

    Respawning Begin

---
+++ Assets/Scripts/RespawnPoint.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Dreamteck.Splines;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+    }
+}
