67c82638468e5fd1fe3ad0763a9d07e6d8eea3bf
DanielOsei-StudentAc <danieloseinimo@gmail.com>
2025-02-14 14:59:01 +0000

    Scene Background Transition

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,10 +1,18 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
 
     public void OpenOptionsMenu()
     {
@@ -15,8 +23,22 @@ public class MenuManager : MonoBehaviour
         Application.Quit();
     }
 
-    public void StartRace()
+    public async void StartRace()
     {
+        await ChangeBackgroundColour(new Color(236, 184, 127));
         SceneManager.LoadScene(1);
+    }
+
+    private async Task ChangeBackgroundColour(Color target)
+    {
+        target /= 255;
+        print(target);
+        while (mainCamera.backgroundColor != target)
+        {
+            mainCamera.backgroundColor = Color.Lerp(mainCamera.backgroundColor, target, 0.5f);
+            print(mainCamera.backgroundColor);
+            await Task.Delay(100);
+        }
+        print("Change complete");
     }
 }
